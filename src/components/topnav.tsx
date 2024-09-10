56e168abff5ad24deccb827335e8e32bd996e86f
Eryk Wójcik <drumpixtv@gmail.com>
2024-09-10 16:08:04 +0200

    updated navbar

--- src/components/topnav.tsx
+++ src/components/topnav.tsx
@@ -1,9 +1,30 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export function TopNav() {
   return (
-    <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold shadow-md">
-      <div>3D Platform</div>
+    <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border">
+      <Link
+        href={"/"}
+        className="flex flex-row items-center justify-center gap-4"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="size-7"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+          />
+        </svg>
+
+        <h1 className="text-xl font-bold">3D Platform</h1>
+      </Link>
       <div>
         <SignedOut>
           <SignInButton />
